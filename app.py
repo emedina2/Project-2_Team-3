d4c2c3e6e5e47b2c6a2fe49fdfac19c85802f005
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-04-26 20:37:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+from login import login, password
 
 from flask import Flask, render_template
+engine = ("postgresql://login:password@localhost:60921/WeatherData")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
 
 app = Flask(__name__)
 
