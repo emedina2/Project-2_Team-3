2c9df3661a7c7737bcd8c223f4cbd2860e4e162c
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-04-27 16:02:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import sqlalchemy
 import psycopg2
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, scoped_session, sessionmaker
 from sqlalchemy import create_engine, func
 from login import login, password
 
 
 from flask import Flask, render_template, redirect
-engine = ('postgresql://${login}:${password}@localhost:60921/WeatherData')
+engine = create_engine('postgresql://${login}:${password}@localhost:5432/WeatherData')
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
