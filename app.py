86dc4e0c6710ef85039851cc29fadc1b8e6323c4
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-04-26 20:11:19 -0500

    Added index.html

--- app.py
+++ app.py
@@ -3,4 +3,10 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return("index.html")
