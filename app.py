d23a2c65f10cd9323d26789759c2ed68b5ad1503
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-04-26 21:17:59 -0500

    Added base files for web page

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import sqlalchemy
+import psycopg2
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from login import login, password
 
-from flask import Flask, render_template
-engine = ("postgresql://login:password@localhost:60921/WeatherData")
+
+from flask import Flask, render_template, redirect
+engine = ('postgresql://${login}:${password}@localhost:60921/WeatherData')
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
@@ -15,3 +17,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
