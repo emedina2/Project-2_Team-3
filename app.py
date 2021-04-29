4b2d727ca91c803d0e1d100806b5358537424bfd
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-04-28 20:18:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template("home.html")
+
+
+@app.route("/Map")
+def index():
     return render_template("index.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
