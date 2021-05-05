8a0af093df311e95f7f28bd5380023bf30925fec
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-05-05 18:34:36 -0500

    Added leaflet JS

--- static/js/app.js
+++ static/js/app.js
@@ -3,16 +3,16 @@ var myMap = L.map("map", {
     zoom: 3
   });
   
-  // Adding a tile layer (the background map image) to our map
-  // We use the addTo method to add objects to our map
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/outdoors-v11",
-    accessToken: API_KEY
-  }).addTo(myMap);
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: 1,
+  id: "mapbox/outdoors-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
 
 
 var monthlyWeatherData = "/api/monthly";
@@ -22,7 +22,7 @@ d3.json(monthlyWeatherData).then(function(response) {
     console.log(response);
     var heatArray = [];
     for (var i = 0; i < response.length; i++) {
-      heatArray.push([response[i].latitude, response[i].longitude]);
+      heatArray.push([parseFloat(response[i].latitude), parseFloat(response[i].longitude)]);
       }
     console.log(heatArray)    
     var heat = L.heatLayer(heatArray, {
