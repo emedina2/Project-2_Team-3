aeaf6339c53ac7a18415d6d394b4e68ab256fb54
aadeba5 <74994708+aadeba5@users.noreply.github.com>
2021-05-04 23:27:35 -0500

    revised

---
+++ static/js/aos.js
@@ -0,0 +1,79 @@
+    <!-- <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <title>Global Weather Changes Throughout The Years</title>
+    
+    Leaflet JS -->
+    <!-- <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+    integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+    crossorigin=""></script>
+    <link rel="stylesheet" href="../Visualization/style.css">
+    </head>
+
+    <body> -->
+    <!-- The div where we will inject our map -->
+    <!-- <div id="map"></div> -->
+    <html><head></head><body>--&gt;
+    <head></head>
+
+    <!-- <script src="https://d3js.org/d3.v5.min.js"></script>
+    <!Animate on Scroll JS Library
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script type="text/javascript" src="../static/js/config.js"></script>
+    JS
+    <script type="text/javascript" src="../static/js/app.js"></script>
+    <script>
+        temperatureMap({{weatherData|tojson}})
+    </script> -->
+    <!-- </body> -->
+    <!-- </html> -->
+
+
+
+  {/* {/* <!-- <script src="https://d3js.org/d3.v5.min.js"></script>
+    // <!Animate on Scroll JS Library -->
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <!API key
+  <!<script type="text/javascript" src="../static/js/config.js"></script>
+  JS
+  <script type="text/javascript" src="../static/js/app.js"></script>
+  <script> */}
+    temperatureMap({{weatherData|tojson}})
+  </script>
+<!</body>
+
+<!</html>&gt;
+
+
+
+
+</body></html>
+
+            
+            
+            // <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+            // <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js"></script>
+
+            // <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+            // <!-- <script src="http://localhost:3002/dist/aos.js"></script> -->
+
+            // <script>
+            //     AOS.init({
+            //         easing: 'ease-out-back',
+            //         duration: 1000
+            //     });
+            // </script>
+
+            // <script>
+            //     hljs.initHighlightingOnLoad();
+
+            //     $('.hero__scroll').on('click', function(e) {
+            //         $('html, body').animate({
+            //             scrollTop: $(window).height()
+            //         }, 1200);
+            //     });
+            // </script>
+        
+
+        // </body> */}
